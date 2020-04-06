cc1e6461814038504064dce7c70d23537bd1d0d5
SpectrumX <wylieza@gmail.com>
2020-04-06 11:43:54 +0200

    Added two ideas to readme

--- README.txt
+++ README.txt
@@ -4,3 +4,7 @@ Tempo Charicteristics to exploit:
  - Tempo is somewhat constant (in best case, constant for entire song)
  - Tempo is periodic -> Auto correlation should spike in accorance to this
  - Beat usually corresponds to a peak in power out
+
+ - The AC result is mirrored, so remove the second half
+ - The AC result is less accurate in the beginning and more accurate nearer the end
+ - If the sample time is too long or two short, the AC result has a higher proportion of problematic information
--- justin_movingavepwr.m
+++ justin_movingavepwr.m
@@ -1,19 +1,37 @@
+%Stats on the go section%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Fortroad - Lost
+%189300 - 158200 -> 31100 %Difference between two adjacent peaks in the auto
+%correlation of moving pwr -> 85.1 bpm (Actual ~85)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Avicii - Hey Brother
+%(241700 - 199300)/2 -> 21200 -> 124.8 bpm (Actual ~125)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Belwoorf - Nostalgia first 5 seconds
+%126100 - 110400 -> 15700 -> 168.5 bpm (Actual is either ~168 or 84)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%djfresh - golddust (Actual ~73 or 145)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%thefatrat - timelapse (Actual ~127)
+
 % MOVING AVERAGE POWER METHOD
 close all; %Close all plots
 
 %Import sound data
 %track_name = 'fortroad_lost.wav';
-%track_name = 'heybrother_avicii.wav';
+track_name = 'heybrother_avicii.wav';
 %track_name = 'thefatrat_timelapse.wav';
-track_name = 'belwoorf_nostalgia.wav';
+%track_name = 'belwoorf_nostalgia.wav';
+%track_name = 'djfresh_golddust.wav';
+
 [x fs]=audioread(track_name);
 
 %Create a time index
 t=0:1/fs:(length(x)-1)/fs;
 
 %'Trim' size of file down to sec seconds duration
-duration = 5; %Choose duration in seconds
-start_time = 33; %Choose start time in seconds
+duration = 10; %Choose duration in seconds
+start_time = 124; %Choose start time in seconds
 
 finnish_time = start_time + duration;
 trimi = find(start_time-1/fs <= t & t <= start_time+1/fs);
@@ -27,7 +45,7 @@ xlim([start_time finnish_time])
 title("Short Section of Sound Data");
 
 %Play sound
-%soundsc(xshort,fs)
+soundsc(xshort,fs)
 
 
 %Moving Average Power
@@ -50,22 +68,29 @@ legend('sound data', 'average power')
 
 %Perform an auto-correlation on the moving ave power
 acpshort = xcorr(pshort);
-%Create a time axis
-tACF=0:1/fs:(length(acpshort)-1)/fs;
 
 %Plot the autocorrelation of the moving ave pwr
 figure
-plot(tACF,acpshort)
+plot(acpshort)
 title("Plot of autocorrelated moving average power")
 xlabel('Time (s)')
 
+
+
+
+
+
 % CALCULATE BPM
+%Create a time axis
+tacpshort = 0:1/fs:(length(acpshort)-1)/fs;
 % Set guidelines on finding the peaks
 minPeakDistance=0.25;
 minPeakHeight=5;
+
 % Find the peaks and locations
 [pks,times] = findpeaks(acpshort,fs,'MinPeakDistance',minPeakDistance,...
     'MinPeakHeight',minPeakHeight);
+
 % Plot points on AC function
 findpeaks(acpshort,fs,'MinPeakDistance',minPeakDistance,...
     'MinPeakHeight',minPeakHeight); 
@@ -79,20 +104,11 @@ disp("Short sound clip samples -> " + length(pshort))
 disp("Auto correlation samples -> " + length(acpshort)) %Is 2N-1
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Fortroad - Lost
-%189300 - 158200 -> 31100 %Difference between two adjacent peaks in the auto
-%correlation of moving pwr -> 85.1 bpm (Actual ~85)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Avicii - Hey Brother
-%(241700 - 199300)/2 -> 21200 -> 124.8 bpm (Actual ~125)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Belwoorf - Nostalgia first 5 seconds
-%126100 - 110400 -> 15700 -> 168.5 bpm (Actual is either ~168 or 84)
-
 
 
 if(0)
+
+
 % Frequency Domain
 
 % Frequency plots
