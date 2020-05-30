124d9ed7ad89f6fac8696685dfe173146b7dc730
SpectrumX <wylieza@gmail.com>
2020-05-30 16:05:28 +0200

    Function to load a track and generate samples array and time indicies

---
+++ f_load_track.m
@@ -0,0 +1,13 @@
+%Loads in a track
+%Takes: [track name]
+%returns: [[track sample values], [sampling frequency], [sample times]]
+
+function [track_sv, track_sf, track_st] = f_load_track(track_name)
+
+%read track
+[track_sv, track_sf]=audioread(track_name);
+
+%generate time indicies
+track_st = 0:1/fs:(length(x)-1)/fs;
+
+end
