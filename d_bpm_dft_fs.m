d11a828c5ae2d918caba5d233be9f79872fab01f
Katie <kathryndvs11@gmail.com>
2020-06-01 14:55:45 +0200

    DFT files with new naming convention

---
+++ d_bpm_dft_fs.m
@@ -0,0 +1,77 @@
+% AUTHOR - Kate
+% Calculate the BPM of a (full) song using frequency domain analysis
+% Requires f_ft_window.m
+
+close all; %Close all plots
+
+%List of tracknames
+%track_name = 'fortroad_lost.wav'; %(Actual ~85)
+track_name = 'heybrother_avicii.wav'; %(Actual ~125)
+%track_name = 'thefatrat_timelapse.wav'; %(Actual ~127)
+%track_name = 'belwoorf_nostalgia.wav'; %(Actual is either ~168 or 84)
+%track_name = 'djfresh_golddust.wav'; %(Actual ~73 or 145)
+%track_name = '80BPMMetronome.wav'; 
+
+%%%%%%CONFIG SETTINGS%%%%%%%%
+%'Trim' size of file down to sec seconds duration
+%duration = 5; %Choose duration in seconds
+duration = 30; %Choose duration in seconds
+start_time = 0; %Choose start time in seconds
+
+min_bpm = 40; 
+max_bpm = 200;
+fs = 44100;
+
+% START: Single window
+%[ft_wind, f] = f_ft_window(track_name, duration, start_time, min_bpm, max_bpm);
+%figure();
+%plot(f, abs(ft_wind))
+%xlim([0 10])
+%title("Frequency response of moving average power")
+%xlabel("Frequency (Hz)")
+%ylabel("Magnitude")
+% Calculate BPM
+%Find dominant frequency
+%maxPeak= max(abs(ft_wind));
+%dominantFreq=f(abs(ft_wind)==maxPeak);
+%BPM = dominantFreq*60;
+% END: Single window
+
+% Get first window
+[ftp, f] = f_ft_window(track_name, duration, start_time, min_bpm, max_bpm);
+
+% Find the FT for each window and sum up the magnitude responses
+while(start_time + 2*duration < track_length(track_name))
+    start_time = start_time + duration;
+    
+    [ftp_i, ~] = f_ft_window(track_name, duration, start_time, min_bpm, max_bpm);
+    
+    %temp fix
+    ftp_i = ftp_i(length(ftp_i) - length(ftp)+1:length(ftp_i));
+    
+    ftp = abs(ftp) + abs(ftp_i);
+    
+end
+
+figure();
+plot(f,ftp)
+xlim([0 10])
+title("Accumulated frequency response of windows")
+xlabel("Frequency (Hz)")
+ylabel("Magnitude")
+
+%Find dominant frequency
+maxPeak= max(ftp);
+dominantFreq=f(ftp==maxPeak);
+BPM = dominantFreq*60;
+
+disp("BPM: " + BPM)
+
+%Functions
+function tk_len = track_length(track_name)
+    
+    [x, fs]=audioread(track_name);
+    tk_len = length(x)/fs;
+
+end
+
---
+++ f_ft_window.m
@@ -0,0 +1,45 @@
+% AUTHOR - Kate
+% Finds the Fourier Transform of a window specified by the starting time
+% and duration
+
+% ft_wind -> Fourier Transform of window (positive frequencies only)
+% f -> corresponding frequency axis
+function [ft_wind, f] = f_ft_window(track_name, duration, start_time, ...
+    min_bpm, max_bpm)
+
+%Read Track
+[x, fs]=audioread(track_name);
+
+%Create a time index
+t=0:1/fs:(length(x)-1)/fs;
+
+%Shorten the sample
+finish_time = start_time + duration;
+trimi = find(start_time-1/fs <= t & t <= start_time+1/fs);
+trimf = find(finish_time-1/fs <= t & t <= finish_time+1/fs);
+xshort=x(trimi:trimf);
+
+% Moving Average of Signal Power for short
+pshort = movmean(xshort.^2, 15);
+
+% FOURIER TRANSFORM
+
+% Frequency axis
+n=-(length(pshort)-1)/2:(length(pshort)-1)/2;
+f=(fs/length(pshort))*n;
+
+% Fourier Transform of the moving ave power
+ftpshort=fftshift(fft(pshort));
+
+% Remove first half (negative frequencies)
+ft_wind= ftpshort(f>=0);
+f=f(f>=0);
+
+% Constraints
+% A song will typically have a tempo between 40 bpm (/min_bpm)
+% and 200 bpm (/max_bpm)
+ft_wind(f<=(min_bpm/60))=0;
+ft_wind(f>=(max_bpm/60))=0;
+
+
+end
